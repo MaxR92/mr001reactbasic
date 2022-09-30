05a6be456d8e06883cb15cc5523f3b5f4a962c55
Max R <Max.Roese@googlemail.com>
2022-09-30 15:57:09 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,9 @@ import _germanNouns from './data/germanNouns.json';
 function shuffle(array) {
 	let currentIndex = array.length,
 		randomIndex;
-
-	// While there remain elements to shuffle.
 	while (currentIndex != 0) {
-		// Pick a remaining element.
 		randomIndex = Math.floor(Math.random() * currentIndex);
 		currentIndex--;
-
-		// And swap it with the current element.
 		[array[currentIndex], array[randomIndex]] = [
 			array[randomIndex],
 			array[currentIndex]
@@ -34,10 +29,15 @@ function App() {
 				{germanNouns.map((germanNoun) => {
 					return (
 						<div className="germanNoun">
-							<div className="singular">
-								{germanNoun.article} {germanNoun.singular}
+							<img src="images/cardback.jpg" />
+							<div className="info">
+								<div className="singular">
+									{germanNoun.article} {germanNoun.singular}
+								</div>
+								<div className="plural">
+									{germanNoun.plural}
+								</div>
 							</div>
-							<div className="plural">{germanNoun.plural}</div>
 						</div>
 					);
 				})}
