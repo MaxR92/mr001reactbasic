53fbaf5483235d5afcb2781d7b5a4b7b1786c7fa
Max R <Max.Roese@googlemail.com>
2022-09-30 14:17:06 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h1>Test Site</h1>
@@ -10,3 +10,4 @@ function App() {
     </div>
   )
 }
+export default App
